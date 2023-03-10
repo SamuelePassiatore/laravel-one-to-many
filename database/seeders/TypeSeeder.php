930b7d12c3cf865ae10242ab4447ee5f699491cd
SamuelePassiatore <semmisno2@protonmail.com>
2023-03-10 15:45:51 +0100

    made seeder with faker for color and foreach on labels

--- database/seeders/TypeSeeder.php
+++ database/seeders/TypeSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Type;
+use Faker\Generator as Faker;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -10,8 +12,14 @@ class TypeSeeder extends Seeder
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
-        //
+        $labels = ['FrontEnd', 'BackEnd', 'FullStack', 'UI/UX', 'Design', 'Blockchain', 'Mobile application', 'Game', 'AI'];
+        foreach ($labels as $label) {
+            $type = new Type();
+            $type->label = $label;
+            $type->color = $faker->hexColor();
+            $type->save();
+        }
     }
 }
